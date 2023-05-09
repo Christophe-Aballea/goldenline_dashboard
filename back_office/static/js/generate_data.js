46a46fdf79716501443b168712f7f2f66de6964b
Christophe ABALLEA <chr.aballea@gmail>
2023-05-09 02:09:46 +0200

    Ajout spinner

--- back_office/static/js/generate_data.js
+++ back_office/static/js/generate_data.js
@@ -2,8 +2,7 @@ document.getElementById("generateDataForm").addEventListener("submit", function(
     const startDateInput = document.getElementById("start_date");
     const startDate = new Date(startDateInput.value);
     const today = new Date();
-    
-    // Assurez-vous que l'heure et les minutes sont réinitialisées pour effectuer une comparaison de date correcte
+
     today.setHours(0, 0, 0, 0);
 
     if (startDate >= today) {
@@ -11,3 +10,16 @@ document.getElementById("generateDataForm").addEventListener("submit", function(
         alert("La date de début doit être antérieure à la date du jour.");
     }
 });
+
+
+document.getElementById('generateDataForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    document.getElementById('loading').style.display = 'block';
+    const submitButton = document.getElementById('submitButton');
+    submitButton.classList.add('disabled');
+    submitButton.disabled = true;
+  
+    // Soumettre le formulaire
+    event.target.submit();
+  });
+  
