e9ca3f6ef60b6555822018f7216b353e859a479b
Christophe ABALLEA <chr.aballea@gmail>
2023-05-11 00:25:19 +0200

    Ajout fonction 'verify_accounts'

--- back_office/modules/accounts.py
+++ back_office/modules/accounts.py
@@ -1,10 +1,38 @@
 import bcrypt
+from random import choice
 
 from back_office.modules.db_utils import create_connection, users_schema
 
 
-def verify_accounts():
-    pass
+async def verify_accounts():
+    conn = None
+    try:
+        message = {}
+
+        # Nombre de comptes super-admin, admin et user
+        get_accounts_query = f"""
+        SELECT email, libelle AS role
+        FROM {users_schema}.users u
+        JOIN {users_schema}.roles r ON u.id_role = r.id_role
+        ORDER BY u.id_role;
+        """
+
+        conn = await create_connection()
+        accounts = await conn.fetch(get_accounts_query)
+
+        for account in accounts:
+            if account["role"] in message:
+                message[account["role"]].append(account["email"])
+            else:
+                message[account["role"]] = [account["email"]]
+
+        print(message)
+        return True, message
+    except Exception as error:
+        return False, [f"Erreur : ", str(error)]
+    finally:
+        if conn:
+            await conn.close()
 
 
 def create_super_admin_account(prenom, nom, email, password):
@@ -36,7 +64,7 @@ VALUES ('{prenom}', '{nom}', '{email}', '{password_hash}', (SELECT id_role FROM 
 
 
 
-async def create_user_account(prenom, nom, email, role, first_login=True, password="non défini"):
+async def create_user_account(prenom, nom, email, role, verification_code=None, first_login=True, password="non défini"):
     conn = None
     try:
         # Récupération des éventuels password_hash liés à l'email de l'utilisateur
@@ -54,13 +82,22 @@ async def create_user_account(prenom, nom, email, role, first_login=True, passwo
         if is_unused or password == "non défini":
             # Génération du hash du mot de passe
             password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-        
+
+            if not verification_code:
+                # Génération d'un code de vérification aléatoire
+                verification_code = [str(choice(range(1, 10)))]
+
+                while len(verification_code) != 4:
+                    digit = str(choice(range(10)))
+                    if digit not in verification_code:
+                        verification_code.append(digit)
+
             # Requête d'ajout d'un compte utilisateur
             insert_account_query = f"""
-            INSERT INTO {users_schema}.users (prenom, nom, email, password_hash, id_role, first_login)
-            VALUES ($1, $2, $3, $4, (SELECT id_role FROM {users_schema}.roles WHERE libelle = $5), FALSE);
+            INSERT INTO {users_schema}.users (prenom, nom, email, password_hash, verification_code, first_login, id_role)
+            VALUES ($1, $2, $3, $4, $5, (SELECT id_role FROM {users_schema}.roles WHERE libelle = $6), $7);
             """        
-            await conn.fetchval(insert_account_query, prenom, nom, email, password_hash, role)
+            await conn.fetchval(insert_account_query, prenom, nom, email, password_hash, int(verification_code), role, first_login)
 
             return True, []
         else:
