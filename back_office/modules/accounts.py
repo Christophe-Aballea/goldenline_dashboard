dcb5c9885045547bc9d2b350af9f1f26f2cd9563
Christophe ABALLEA <chr.aballea@gmail>
2023-05-16 12:47:48 +0200

    Ajout fonction activate_user()

--- back_office/modules/accounts.py
+++ back_office/modules/accounts.py
@@ -53,16 +53,16 @@ def create_superadmin_account(prenom, nom, email, password):
 
     # Requêtes SQL pour insérer les données
     insert_roles = """
-INSERT INTO roles (libelle) VALUES
-    ('superadmin'),
-    ('admin'),
-    ('user');
-"""
+    INSERT INTO roles (libelle) VALUES
+        ('superadmin'),
+        ('admin'),
+        ('user');
+    """
 
     insert_superadmin = f"""
-INSERT INTO users (prenom, nom, email, password_hash, id_role, first_login)
-VALUES ('{prenom}', '{nom}', '{email}', '{password_hash}', (SELECT id_role FROM roles WHERE libelle = 'superadmin'), FALSE);
-"""
+    INSERT INTO users (prenom, nom, email, password_hash, id_role, first_login)
+    VALUES ('{prenom}', '{nom}', '{email}', '{password_hash}', (SELECT id_role FROM roles WHERE libelle = 'superadmin'), FALSE);
+    """
 
     # Création du fichier 'create_superadmin_user.sql'
     try:
@@ -163,4 +163,69 @@ async def get_users_by_roles(current_user_id_role):
         if conn:
             await conn.close()
        
+    return success, message
+
+# Récupération des informations d'un compte en fonction de l'email
+async def get_login_type_from_email(email):
+    users_schema = get_users_schema()
+    message = []
+    conn = None
+    try:
+        get_user_infos_query = f"""
+        SELECT id_user, email, password_hash, id_role, first_login, verification_code
+        FROM {users_schema}.users
+        WHERE email = '{email}';
+        """
+        conn = await create_connection()
+        user = await conn.fetchrow(get_user_infos_query)
+        print(user)
+        if user is None:
+            success = False
+            message.append("Identifiant, mot de passe ou code d'activation incorrect")
+        else:
+            success = True
+            message  = user["first_login"]
+    except Exception as error:
+        success = False
+        message.append(f"Impossible de vérifier la validité des informations de connexion : {str(error)}")
+    finally:
+        if conn:
+            await conn.close()
+       
+    return success, message
+
+
+# Activation d'un compte
+# - suppression du code d'activation
+# - 'first_login' -> False
+# - hashage et enregistrement du password
+async def activate_user(email, password):
+    users_schema = get_users_schema()
+    message = []
+    success = False
+    conn = None
+    try:
+        # Hashage du password
+        password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+        # Requête d'activation de l'utilisateur
+        activate_user_update_query = f"""
+        UPDATE {users_schema}.users
+        SET (password_hash, verification_code, first_login) = ('{password_hash}', ' ', false)
+        WHERE email = '{email}';
+        """
+        conn = await create_connection()
+        result = await conn.fetchval(activate_user_update_query)
+        print(result)
+        if result:
+            success = True
+            message.append(f"Compte {email} activé avec succès")
+        else:
+            success = False
+            message.append(f"Impossible d'activer le compte {email}")
+    except Exception as error:
+        message.append(f"Erreur : {str(error)}")
+    finally:
+        if conn:
+            await conn.close()
     return success, message
