1e290b984b22dd53c4300f1d8608b4d50d9154a7
Christophe ABALLEA <chr.aballea@gmail>
2023-05-17 09:03:10 +0200

    Export get_db() '/api/api_routes' -> '/db/database.py'

--- api/routers/api_routes.py
+++ api/routers/api_routes.py
@@ -1,21 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from db.database import SessionLocal
+from db.database import get_db
 from typing import List
 
 from api.models import Collecte, Achat
 from api.schemas import Depense
 
 router = APIRouter()
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.get("/depense", response_model=List[Depense])
