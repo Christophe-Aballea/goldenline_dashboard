7c0ec031d1290625061ce62b3b8e56fbcd5690de
Christophe ABALLEA <chr.aballea@gmail>
2023-05-21 23:26:33 +0200

    Ajout fonction run_in_db_session()

--- db/database.py
+++ db/database.py
@@ -1,3 +1,4 @@
+from contextvars import ContextVar
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,6 +12,10 @@ DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Création d'un ContextVar pour stocker une session de base de données
+db_session_var = ContextVar("db_session", default=None)
+
+
 # Dépendance pour routes
 def get_db():
     db = SessionLocal()
@@ -18,3 +23,25 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# Lance la fonction asynchrone 'function' dans un contextvar
+# et renvoie le résultat à la fonction appelante
+# Permet d'appeler depuis le front une fonction asynchrone faisant appel à la base de données
+# sans avoir à créer de session dans le front
+async def run_in_db_session(function, *args, **kwargs):
+    db = SessionLocal()
+    print(f"SessionLocal() returned {db}")  # add this for debugging
+    try:
+        db_session_var.set(db)
+        print(f"db_session_var is now {db_session_var.get()}")  # add this for debugging
+        result = await function(*args, **kwargs)
+        return result
+    except Exception as error:
+        import traceback
+        print(f"Erreur : {type(error)}")
+        print(f"Message d'erreur : {str(error)}")
+        traceback.print_exc()
+    finally:
+        db.close()
+
